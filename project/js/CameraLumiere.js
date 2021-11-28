b2946a84e71787ab6611f08ed4897262b1dd553a
DjTrustW <guyothug@gmail.com>
2021-11-28 08:42:11 +0100

    Update CameraLumiere.js
    j ai épurer.

--- project/js/CameraLumiere.js
+++ project/js/CameraLumiere.js
@@ -1,33 +1,17 @@
  var xDir=0;
  var yDir=0;
- var zDir=0.25;
+ var zDir=1;
   
- function cameraLumiere(scene,camera){   // creation de la camera 
+ function cameraLumiere(scene,camera){
   camera.up = new THREE.Vector3( 0, 0, 1 );
-  let xPos=3;
-  let yPos=3;
-  let zPos=3;
+  let xPos=50;
+  let yPos=50;
+  let zPos=50;
   camera.position.set(xPos, yPos, zPos);
   camera.lookAt(xDir, yDir, zDir);
   camera.updateProjectionMatrix();
-  actuaPosCameraHTML(camera);
-} // fin fonction cameraLumiere
-
- function actuaPosCameraHTML(camera){
-  document.forms["controle"].PosX.value=camera.position.x;
-  document.forms["controle"].PosY.value=camera.position.y;
-  document.forms["controle"].PosZ.value=camera.position.z;
-  document.forms["controle"].DirX.value=xDir;
-  document.forms["controle"].DirY.value=yDir;
-  document.forms["controle"].DirZ.value=zDir;
- }
+}
  
-//*************************************************************
-//* 
-//        F I N     C A M E R A
-//
-//*************************************************************
-
  function lumiere(scene){
     let lumPt = new THREE.PointLight(0xff55ff);
     lumPt.position.set(3,3,-3);
@@ -42,4 +26,4 @@
     lumPt1.position.set(5,-15,15);
     lumPt1.intensity = 1;
     scene.add(lumPt1);
-}// fin fonction lumiere
+}
